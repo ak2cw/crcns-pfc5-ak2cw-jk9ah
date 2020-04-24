c27a15328450c517fa690ebd14f5205912380ef5
Amrutha Kadali <amrutha@Amruthas-MacBook-Pro.local>
2020-04-24 15:00:48 -0400

    updated READMEs and io.py

--- data/README.md
+++ data/README.md
@@ -1,3 +1,9 @@
 
 Edit this file to describe how to retrieve the data set. Except for very small data files, it's not recommended to check data into version control.
 We downloaded the data from the pfc-5 dataset for each trial and kept the .mat file. We renamed all the files to data_derived#.mat. We did this by downloading each file individually from the CRCNS website. 
+
+The data set can be found at https://crcns.org/data-sets/pfc/pfc-5/about-pfc-5, in the data directory. 
+Download all the tar.gz files in data and unzip them. 
+When you run the src io.py file, put this into terminal: python io.py filepath
+filepath should be replaced with your file path to the data folder, for example /Users/amrutha/crcns-pfc5-ak2cw-jk9ah/data/
+it needs the last slash to work. This will run the code for generating all of our graphs, they will save to the src folder. 
--- src/io.py
+++ src/io.py
@@ -3,18 +3,36 @@
 """Functions for file IO"""
 from __future__ import print_function, division, absolute_import
 import scipy.io
-
-
+import matplotlib.pyplot as plt
+import sys
 import h5py
 import numpy as np
 from scipy.io import loadmat
 
 
 def read_neuron_trial(fp): #returns the EEG data during one trial at different time points of exp
-    d = fp['trial']
-    o = d[0][0]
-    trialdata = fp[o][:]
-    return trialdata
+    d = fp['trial'] #get trials data
+    numtimes = 0
+    for x in d: #get number of time points
+        o = x[0]
+        y = fp[o][:]
+        numtimes = len(y)
+        break
+
+
+    sums = np.zeros(numtimes) #array for collecting data from each trial
+
+    for trial in d: #for every trial
+        o = trial[0]
+        trialdata = fp[o][:]
+        for i in range(0, len(trialdata)): #for every time point
+            row = trialdata[i]
+            avg = np.mean(row[0:64])
+
+            sums[i] = sums[i] + avg #adding the average of 64 electrode values to for each time point
+    avgs = []
+    avgs = sums/len(d) #the average electrode value at a given time point across all trials
+    return avgs
 
 
 def read_neuron_time(fp): #returns the time stamps for the EEG data
@@ -24,31 +42,198 @@ def read_neuron_time(fp): #returns the time stamps for the EEG data
     return timedata
 
 
-def read_neuron_trialinfo(fp): #returns information about trial - documented on CRCNS
-    trialinfo = fp['trialinfo']
-    return trialinfo
+def getpersonavgs_ctrl(group, filep):
+
+    allctrls = np.zeros(385)
+
+    if (group == 0): #if all control
+        for i in range (1,21): #runs for all subjects
+                if ( i < 10):
+                    filepath = filep +  "ctrl0%s/data_derived.mat"%(i)
+                if (i >= 10):
+                    filepath = filep +  "ctrl%s/data_derived.mat"%(i)
+                hf = h5py.File(filepath, 'r')
+
+                #retrieves trial data for each stage of experiment
+                enctrialsavgs = read_neuron_trial(hf['encmain'])
+
+                #retrieves time stamp data
+                enctimes = read_neuron_time(hf['encmain'])
+
+                #add on every trial
+                allctrls = allctrls + enctrialsavgs
+
+        allctrls = allctrls/21 #average across all controls
+        return allctrls, enctimes
+
+    mctrls = np.zeros(385)
+    if (group == 1):
+        males = [3,4,5,7,8,9,10,12,13,16,20]
+        for i in males: #runs for some subjects - males
+
+                if ( i < 10):
+                    filepath = filep +  "ctrl0%s/data_derived.mat"%(i)
+                if (i >= 10):
+                    filepath = filep +  "ctrl%s/data_derived.mat"%(i)
+                hf = h5py.File(filepath, 'r')
+
+                #retrieves trial data for each stage of experiment
+                enctrialsavgs = read_neuron_trial(hf['encmain'])
+
+                #retrieves time stamp data
+                enctimes = read_neuron_time(hf['encmain'])
+
+                #add on every trial
+                mctrls = mctrls + enctrialsavgs
+
+        mctrls = mctrls/11 #average across male controls
+        return mctrls, enctimes
+
+    fctrls = np.zeros(385)
+    if(group == 2):
+        females = [1,2,6,11,14,15,17,18,19]
+        for i in females: #runs for some subjects - females
+
+                if ( i < 10):
+                    filepath = filep +  "ctrl0%s/data_derived.mat"%(i)
+                if (i >= 10):
+                    filepath = filep +  "ctrl%s/data_derived.mat"%(i)
+                hf = h5py.File(filepath, 'r')
+
+                #retrieves trial data for each stage of experiment
+                enctrialsavgs = read_neuron_trial(hf['encmain'])
+
+                #retrieves time stamp data
+                enctimes = read_neuron_time(hf['encmain'])
+
+                #add on every trial
+                fctrls = fctrls + enctrialsavgs
+
+        fctrls = fctrls/9 #average across male controls
+        return fctrls, enctimes
+
+    return 0,0
 
 
 
-for i in range (1,4): #runs for subset of subjects
-    filepath = '/Users/amrutha/crcns-pfc5-ak2cw-jk9ah/data/data_derived%s.mat' % (i)
-    hf = h5py.File(filepath, 'r')
+def getpersonavgs_pfc(group):
 
 
-    #retrieves trial data for each stage of experiment
-    enctrials = read_neuron_trial(hf['encmain'])
-    pretrialtrials = read_neuron_trial(hf['pretrial'])
-    proctrials = read_neuron_trial(hf['proc'])
+    allpfcs = np.zeros(385)#231 for proc
 
-    #retrieves time stamp data
-    enctimes = read_neuron_time(hf['encmain'])
-    pretrialtimes = read_neuron_time(hf['pretrial'])
-    proctimes = read_neuron_time(hf['proc'])
 
-    #retrieves trial info
-    enctrialinfo = read_neuron_trialinfo(hf['encmain'])
-    pretrialtrialinfo = read_neuron_trialinfo(hf['pretrial'])
-    proctrialinfo = read_neuron_trialinfo(hf['proc'])
+    if (group == 0): #if all pfc
+        for i in range (1,15): #runs for all subjects
+            if ( i < 10):
+                    filepath = filep +  "pfc0%s/data_derived.mat"%(i)
+            if (i >= 10):
+                    filepath = filep +  "pfc%s/data_derived.mat"%(i)
+            hf = h5py.File(filepath, 'r')
+
+
+            #retrieves trial data for each stage of experiment
+            enctrialsavgs = read_neuron_trial(hf['encmain'])
+
+            #retrieves time stamp data
+            enctimes = read_neuron_time(hf['encmain'])
+
+            #add on every trial
+            allpfcs = allpfcs + enctrialsavgs
+
+        allpfcs = allpfcs/14 #average across all pfc
+        return allpfcs, enctimes
+
+    mpfcs = np.zeros(385)
+    if (group == 1):
+        males = [2, 3, 4, 10, 11]
+        for i in males: #runs for some subjects - males
+
+            if ( i < 10):
+                    filepath = filep + "pfc0%s/data_derived.mat"%(i)
+            if (i >= 10):
+                    filepath = filep + "pfc%s/data_derived.mat"%(i)
+            hf = h5py.File(filepath, 'r')
+
+
+            #retrieves trial data for each stage of experiment
+            enctrialsavgs = read_neuron_trial(hf['encmain'])
+
+            #retrieves time stamp data
+            enctimes = read_neuron_time(hf['encmain'])
+
+            #add on every trial
+            mpfcs = mpfcs + enctrialsavgs
+
+        mpfcs = mpfcs/5 #average across all pfc
+        return mpfcs, enctimes
+
+    fpfcs = np.zeros(385)
+    if(group == 2):
+        females = [1, 5, 6, 7, 8, 9, 12, 13, 14]
+        for i in females: #runs for some subjects - females
+
+            if ( i < 10):
+                    filepath = filep +  "pfc0%s/data_derived.mat"%(i)
+            if (i >= 10):
+                    filepath = filep +  "pfc%s/data_derived.mat"%(i)
+            hf = h5py.File(filepath, 'r')
+
+
+            #retrieves trial data for each stage of experiment
+            enctrialsavgs = read_neuron_trial(hf['encmain'])
+
+            #retrieves time stamp data
+            enctimes = read_neuron_time(hf['encmain'])
+
+            #add on every trial
+            fpfcs = fpfcs + enctrialsavgs
+
+        fpfcs = fpfcs/9 #average across all pfc
+        return fpfcs, enctimes
+
+    return 0,0
+
+
+
+
+def main():
+
+    filepath = sys.argv[1]
+
+
+    cs, timec = getpersonavgs_ctrl(0, filepath) #run for controls#
+    ps, timep = getpersonavgs_pfc(0, filepath) #run for pfc
+    plt.plot(timec, cs)
+    plt.plot(timep, ps)
+    plt.savefig('control_pfc_avgs.png')
+
+    plt.clf()
+    plt.cla()
+    plt.close() #reset plt
+
+
+    mc, timec = getpersonavgs_ctrl(1, filepath) #run for male controls
+    fc, timec = getpersonavgs_ctrl(2, filepath)# run for female controls
+    plt.plot(timec, mc)
+    plt.plot(timec,fc)
+    plt.savefig('control_gender.png')
+
+    plt.clf()
+    plt.cla()
+    plt.close() #reset plt
+
+    mp, timec = getpersonavgs_pfc(1, filepath) #run for male pfcs
+    fp, timec = getpersonavgs_pfc(2, filepath)# run for female pfcs
+    plt.plot(timec, mp)
+    plt.plot(timec,fp)
+    plt.savefig('pfc_gender.png')
+
+if __name__ == '__main__':
+    main()
+
+
+
+
 
 
     #moving forward inside this for loop, what will we do with this data.
